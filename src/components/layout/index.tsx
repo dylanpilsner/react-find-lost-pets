4fdcda62c21d4e80981a8bf5efc2d0f64a7720bd
Dylan Pilsner <dpilsner@cetrogar.com.ar>
2022-12-13 14:22:22 -0300

    test

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "../header";
 import css from "./layout.css";
+import { Title } from "../../ui/texts";
 
 function Layout() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Title>Mascotas perdidas cerca tuyo</Title>
+    </div>
+  );
 }
 
 export { Layout };
---
+++ src/ui/texts/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title({ children }) {
+  return <h1>{children}</h1>;
+}
+
+function Paragraph({ children }) {
+  return <p>{children}</p>;
+}
+
+export { Title };
