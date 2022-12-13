17f36bb20334667f71fad8119f3efe42e282a52d
Dylan Pilsner <dpilsner@cetrogar.com.ar>
2022-12-13 11:26:34 -0300

    testeando hook ref

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import css from "./header.css";
 
 function Header() {
+  const navMenu: any = useRef();
+
+  function openNavMenu(e) {
+    navMenu.current.classList.toggle(css.opened);
+  }
+  function closeNavMenu(e) {
+    navMenu.current.classList.toggle(css.closed);
+  }
+
   return (
     <header className={css.header}>
       <img className={css.logo} src="./src/assets/logo.png" />
-      <div className={css["open-nav-menu"]}>
+      <div className={css["open-nav-menu"]} onClick={openNavMenu}>
         <div className={css["hamburger-top-bread"]}></div>
         <div className={css["hamburger-top-patty"]}></div>
         <div className={css["hamburger-bottom-bread"]}></div>
       </div>
-      <nav className={css["nav-menu"]}>
-        <img className={css["close-nav-menu"]} src="./src/assets/close.png" />
+      <nav ref={navMenu} className={css["nav-menu"]}>
+        <img
+          className={css["close-nav-menu"]}
+          src="./src/assets/close.png"
+          onClick={openNavMenu}
+        />
         <nav className={css["nav-item my-data"]}>Mis datos</nav>
         <nav className={css["nav-item my-reported-pets"]}>
           Mis mascotas reportadas
