6035f293226e1b07ddbdb355d9365cc703f2c3ae
Dylan Pilsner <dpilsner@cetrogar.com.ar>
2022-12-13 13:05:17 -0300

    header finalizado

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,36 @@
 import React, { useState, useRef } from "react";
-import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import css from "./header.css";
 
 function Header() {
+  const navigateHook = useNavigate();
   const navMenu: any = useRef();
 
   function openNavMenu(e) {
+    const hasClosedClass = navMenu.current
+      .getAttribute("class")
+      .includes(css.closed);
     navMenu.current.classList.toggle(css.opened);
+    if (hasClosedClass) {
+      navMenu.current.classList.remove(css.closed);
+    }
   }
   function closeNavMenu(e) {
+    navMenu.current.classList.toggle(css.opened);
     navMenu.current.classList.toggle(css.closed);
+  }
+
+  function navigate(path) {
+    navigateHook(path);
   }
 
   return (
     <header className={css.header}>
-      <img className={css.logo} src="./src/assets/logo.png" />
+      <img
+        className={css.logo}
+        src="./src/assets/logo.png"
+        onClick={() => navigate("/")}
+      />
       <div className={css["open-nav-menu"]} onClick={openNavMenu}>
         <div className={css["hamburger-top-bread"]}></div>
         <div className={css["hamburger-top-patty"]}></div>
@@ -24,18 +40,28 @@ function Header() {
         <img
           className={css["close-nav-menu"]}
           src="./src/assets/close.png"
-          onClick={openNavMenu}
+          onClick={closeNavMenu}
         />
-        <nav className={css["nav-item my-data"]}>Mis datos</nav>
-        <nav className={css["nav-item my-reported-pets"]}>
+        <nav className={css["nav-item"]} onClick={() => navigate("/test")}>
+          Mis datos
+        </nav>
+        <nav
+          className={css["nav-item"]}
+          onClick={() => navigate("my-reported-pets")}
+        >
           Mis mascotas reportadas
         </nav>
-        <nav className={css["nav-item report-pet"]}>Reportar mascota</nav>
+        <nav
+          className={css["nav-item"]}
+          onClick={() => navigate("/report-pet")}
+        >
+          Reportar mascota
+        </nav>
         <div className={css["account-container"]}>
           <img className={css["user-img"]} src="./src/assets/user.png" />
-          <span className={css.user}>test</span>
+          <span className={css.user}>Invitadx</span>
         </div>
-        <div className={css.sign}>test</div>
+        <div className={css.sign}>INICIAR SESIÓN</div>
       </nav>
     </header>
   );
